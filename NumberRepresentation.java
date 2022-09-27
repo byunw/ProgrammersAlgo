679d409dfe37cf595f5007f4db5fc85f5b11cb88
byunw <wbyun1461@gmail.com>
2022-09-27 13:33:57 +0900

    Create NumberRepresentation.java

---
+++ NumberRepresentation.java
@@ -0,0 +1,44 @@
+package ProgrammersAlgo;
+
+class Solution {
+      public int solution(int n){
+             
+             int answer = 0;
+             
+             for(int i=1;i<=n;++i){
+                
+                if(i==n){
+                   answer+=1;
+                   break;
+                }
+  
+                int sequenceSum = i;
+               
+                for(int y=(i+1);y<=(n-1);++y){
+                   
+                   if(sequenceSum<n){
+                      sequenceSum+=y;
+                      if(sequenceSum==n){
+                         answer+=1;
+                         break;
+                      }
+                      if(sequenceSum>n){
+                         break;
+                      }
+                   }
+                }
+               
+            }
+            return answer;
+    }
+    
+}
+
+public class NumberRepresentation{
+
+       public static void main(String[] args){
+           Solution a = new Solution();
+           System.out.println(a.solution(15));
+       }
+       
+}
