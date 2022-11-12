e19f9a4975c1ff37790089346a81f7e9a171c28d
byunw <wbyun1461@gmail.com>
2022-11-12 21:41:25 +0900

    Create Age.java

---
+++ Age.java
@@ -0,0 +1,15 @@
+class Solution{
+    
+      public static int solution(int age){
+             return 2022-age+1;
+      }
+      
+}
+
+public class Age{
+        
+       public static void main(String[] args){
+              System.out.println(Solution.solution(23));
+       }
+  
+}
